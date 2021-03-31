f8e891a1236382919800067a88a60e88fa995289
cloudWarMeng@outlook.com <cloudWarMeng@outlook.com>
2021-03-31 21:27:04 +0800

    int c =100;

--- main.cc
+++ main.cc
@@ -4,6 +4,7 @@ int main(int argc,char* argv[]) {
 
     int a=20;
     int b=30;
+    int c =100;
     std::cout<<a<<" + "<<b<<" = "<<add(a,b)<<std::endl;
     std::cout<<"hello world"<<std::endl;
     return 0;
