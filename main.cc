93e5e904c4369c1c3a5fbee6fb5627249aa715ea
cloudWarMeng@outlook.com <cloudWarMeng@outlook.com>
2021-03-31 21:31:03 +0800

    include math.h

--- main.cc
+++ main.cc
@@ -1,7 +1,9 @@
 #include <iostream>
-
+#include "math.h"
 int main(int argc,char* argv[]) {
-
+    int a =100;
+    int b =200;
+    int c = add(a,b);
     std::cout<<"hello world"<<std::endl;
     return 0;
 }
